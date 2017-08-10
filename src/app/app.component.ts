ca32e12c009996c5fd68051465141af953cff438
sterben13 <ivan.dev.job@gmail.com>
2017-08-10 16:08:59 -0500

    http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,3 @@
-/*import { Component, OnInit } from '@angular/core';
-import { Hero } from './class/hero';
-import { HeroService } from './services/hero.service';
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit {
-  title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  ngOnInit() {
-    this.getHeroes();
-  }
-  constructor(private heroService: HeroService) { }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().then((heroes) => this.heroes = heroes);
-  }
-}*/
-
 import { Component } from '@angular/core';
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
@@ -8,8 +9,12 @@ import { HeroService } from './services/hero.service';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './components/hero-search/hero-search.component';
+import { HeroSearchService } from './services/hero-search.service';
 
 @NgModule({
   declarations: [
@@ -17,13 +22,16 @@ import { AppRoutingModule } from './app-routing.module';
     HeroDetailComponent,
     DashboardComponent,
     HeroesComponent,
+    HeroSearchComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
-  providers: [HeroService],
+  providers: [HeroService, HeroSearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
